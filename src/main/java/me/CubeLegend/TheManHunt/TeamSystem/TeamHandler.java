21f18e9e860896b0fb0f096d57c115916590d4d5
CubeLegend <torgehansen17@gmail.com>
2022-09-14 18:31:59 +0200

    removed old world counting from registerScoreBoard()

--- src/main/java/me/CubeLegend/TheManHunt/TeamSystem/TeamHandler.java
+++ src/main/java/me/CubeLegend/TheManHunt/TeamSystem/TeamHandler.java
@@ -86,17 +86,13 @@ public class TeamHandler implements Listener {
         }
     }
 
-    private int worldCount = 0;
-
     public void registerScoreBoard() {
-        if (++worldCount >= 3) {
-            manager = Bukkit.getScoreboardManager();
-            assert manager != null;
-            scoreBoard = manager.getNewScoreboard();
-            TeamHandler.getInstance().createTeam("Runners", "diamond_shovel", 3, "BLUE");
-            TeamHandler.getInstance().createTeam("Hunters", "diamond_sword", 6, "RED");
-            TeamHandler.getInstance().createTeam("Spectators", "gray_stained_glass", 5, "GRAY");
-        }
+        manager = Bukkit.getScoreboardManager();
+        assert manager != null;
+        scoreBoard = manager.getNewScoreboard();
+        TeamHandler.getInstance().createTeam("Runners", "diamond_shovel", 3, "BLUE");
+        TeamHandler.getInstance().createTeam("Hunters", "diamond_sword", 6, "RED");
+        TeamHandler.getInstance().createTeam("Spectators", "gray_stained_glass", 5, "GRAY");
     }
 
     @EventHandler
